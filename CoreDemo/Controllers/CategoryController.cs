de43a9642834af43eb082306c4572892d92a564a
özge şahbaz <ozgeesahbazz@gmail.com>
2022-02-17 16:26:16 +0300

    proje eklendi.

--- CoreDemo/Controllers/CategoryController.cs
+++ CoreDemo/Controllers/CategoryController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
-    public class CategoryyController : Controller
+    public class CategoryController : Controller
     {
         public IActionResult Index()
         {
